38324087eecf7781b8f318008f0c20f0905ad882
IngrateSihombing <ingratejoy@gmail.com>
2026-03-03 10:21:05 +0700

    Enhance Course class with attributes and methods
    Added attributes and methods for course details.

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,38 @@
 package academic.model;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
 public class Course {
+    private String code;
+    private String name;
+    private int credits;
+    private String grade; // Diasumsikan segmen keempat adalah nilai/grade
 
-    // class definition
+    public Course(String code, String name, int credits, String grade) {
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+        this.grade = grade;
+    }
 
+    // Metode getter untuk mengakses atribut course
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    // Override toString() untuk format output yang diminta
+    @Override
+    public String toString() {
+        return code + "|" + name + "|" + credits + "|" + grade;
+    }
 }
