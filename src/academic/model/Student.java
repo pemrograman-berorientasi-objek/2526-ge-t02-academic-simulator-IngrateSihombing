1d3c506e0b9b77edbc67e6c6926ba6a183989e66
IngrateSihombing <ingratejoy@gmail.com>
2026-03-03 10:22:35 +0700

    Enhance Student class with attributes and getters
    Added attributes and methods for Student class.

--- src/academic/model/Student.java
+++ src/academic/model/Student.java
@@ -1,11 +1,38 @@
 package academic.model;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
 public class Student {
+    private String id;
+    private String name;
+    private int year; // Diasumsikan tahun angkatan adalah integer
+    private String studyProgram;
 
-    // class definition
+    public Student(String id, String name, int year, String studyProgram) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.studyProgram = studyProgram;
+    }
 
+    // Metode getter untuk mengakses atribut student
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getStudyProgram() {
+        return studyProgram;
+    }
+
+    // Override toString() untuk format output yang diminta
+    @Override
+    public String toString() {
+        return id + "|" + name + "|" + year + "|" + studyProgram;
+    }
 }
